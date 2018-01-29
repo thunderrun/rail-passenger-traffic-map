0a9968c21651b836cc34ed445147c7d871f86092
thunderrun <thunderrun2035@gmail.com>
2018-01-29 22:54:02 +0800

    add text

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const getSubwayData = () => fetch('3201_drw_nanjing.json')
   .then(res => res.json())
   .then(res => res.l);
 
-getSubwayData().then((data) => {
+const drawLines = (data) => {
   data.forEach(line => {
     let pathString = '';
-    line.c.forEach(drawPoint=>{
+    line.c.forEach(drawPoint => {
       pathString += ` L ${drawPoint}`
     })
     let path = new fabric.Path(pathString, {
@@ -17,5 +17,36 @@ getSubwayData().then((data) => {
     });
     canvas.add(path);
   })
+};
 
+const drawStations = (data) => {
+  let circles = [];
+  let texts = [];
+  data.forEach(line => {
+    line.st.forEach(station => {
+      console.log(station)
+      let coordinates = station.p.split(' ');
+      circles.push(new fabric.Circle({
+        radius: 5,
+        fill: 'white',
+        left: Number(coordinates[0])-3,
+        top: Number(coordinates[1])-3,
+        borderColor: 'black',
+        stroke: 'black', 
+        strokeWidth: 2,
+      }));
+      texts.push(new fabric.Text(station.n, {
+        fontSize: 10,
+        left: Number(coordinates[0])+10,
+        top: Number(coordinates[1])+10
+      }))
+    });
+  });
+  canvas.add(...circles);
+  canvas.add(...texts);
+};
+
+getSubwayData().then((data) => {
+  drawLines(data);
+  drawStations(data);
 });
