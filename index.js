b87af2fd5951f61ffc5e62fceb46dd0a684e5517
thunderrun <thunderrun2035@gmail.com>
2018-02-01 23:18:29 +0800

    add shanghai

--- index.js
+++ index.js
@@ -1,51 +1,74 @@
 const canvas = new fabric.Canvas('canvas');
 
-const getSubwayData = () => fetch('3201_drw_nanjing.json')
+const getSubwayData = (city) => fetch(`data/${city}.json`)
   .then(res => res.json())
   .then(res => res.l);
 
-const drawLines = (data) => {
-  data.forEach(line => {
-    let pathString = '';
-    line.c.forEach(drawPoint => {
-      pathString += ` L ${drawPoint}`
-    })
-    let path = new fabric.Path(pathString, {
-      fill: '',
-      stroke: `#${line.cl}`,
-      strokeWidth: 5
-    });
-    canvas.add(path);
+const createPathObj = (line) => {
+  let pathString = '';
+  line.c.forEach(drawPoint => {
+    pathString += ` L ${drawPoint}`
   })
+  return new fabric.Path(pathString, {
+    fill: '',
+    stroke: `#${line.cl}`,
+    strokeWidth: 5
+  });
 };
 
-const drawStations = (data) => {
+const createCircleObj = (station) => {
+  let coordinates = station.p.split(' ');
+  return new fabric.Circle({
+    radius: 4,
+    fill: 'white',
+    left: Number(coordinates[0]) - 2,
+    top: Number(coordinates[1]) - 2,
+    borderColor: 'black',
+    stroke: 'black',
+    strokeWidth: 1.5,
+  });
+};
+
+const createTextObj = (station) => {
+  let coordinates = station.p.split(' ');  
+  return new fabric.Text(station.n, {
+    fontSize: 8,
+    left: Number(coordinates[0]) + 10,
+    top: Number(coordinates[1]) - 2,
+  });
+};
+
+const createObjects = (data) => {
+  let lines = []
   let circles = [];
   let texts = [];
   data.forEach(line => {
+    lines.push(createPathObj(line))
     line.st.forEach(station => {
-      let coordinates = station.p.split(' ');
-      circles.push(new fabric.Circle({
-        radius: 4,
-        fill: 'white',
-        left: Number(coordinates[0])-2,
-        top: Number(coordinates[1])-2,
-        borderColor: 'black',
-        stroke: 'black', 
-        strokeWidth: 1,
-      }));
-      texts.push(new fabric.Text(station.n, {
-        fontSize: 8,
-        left: Number(coordinates[0]) + 10,
-        top: Number(coordinates[1]) - 2,
-      }))
+      circles.push(createCircleObj(station));
+      texts.push(createTextObj(station));
     });
   });
-  canvas.add(...circles);
-  canvas.add(...texts);
+  return [...lines, ...circles, ...texts]
 };
 
-getSubwayData().then((data) => {
-  drawLines(data);
-  drawStations(data);
+const drawSubwayMap = (city, offset) => {
+  getSubwayData(city).then((data) => {
+    let objects = createObjects(data);
+    let group = new fabric.Group(objects, {
+      left: offset.x,
+      top: offset.y
+    });
+    canvas.add(group);
+  });
+};
+
+drawSubwayMap('nanjing', {
+  x: 0,
+  y: 0
+});
+
+drawSubwayMap('shanghai', {
+  x: 2000,
+  y: 0
 });
