1ffbe3f47d7bbd4efd09d2907bbffe69f6f87a4b
zouyun <thunderrun2035@gmail.com>
2018-03-19 17:57:45 +0800

    add station coordinates

--- subway.js
+++ subway.js
@@ -1,52 +1,72 @@
 const getCityNameInUrl = () => {
   const currentUrlSearch = window.location.search;
   const searchParams = new URLSearchParams(currentUrlSearch);
-  const cityName = searchParams.get('city');
+  const cityName = searchParams.get("city");
   return cityName;
-}
+};
 
-const getCityAdcodeByCityName = (cityName) => {
-  return fetch('/data/cityToAdcode.json').then((response) => {
-    return response.json();
-  }).then((cityAdcode) => {
-    return cityAdcode[cityName];
-  });
-}
+const getCityAdcode = cityName => {
+  return fetch("/data/cityToAdcode.json")
+    .then(response => {
+      return response.json();
+    })
+    .then(cityAdcode => {
+      return cityAdcode[cityName];
+    });
+};
 
-const getSubwayInfo = async (cityName, cityAdcode) => {
+const getSubwayInfo = (cityName, cityAdcode) => {
   return fetch(
     `http://webapi.amap.com/subway/data/${cityAdcode}_drw_${cityName}.json?uid=1521018060609`
-  ).then(response => {
-    return response.json();
-  }).then(json => {
-    return json;
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      return json;
+    });
+};
+
+const getStationsCoordinates = subwayInfo => {
+  let stationsCoordinates = {};
+  subwayInfo.l.forEach(line => {
+    line.st.forEach(station => {
+      stationsCoordinates[station.sid] = station.sl;
+    });
   });
-}
+  return stationsCoordinates;
+};
+
+const getStationsIndex = () => {};
 
 window.cbk = async () => {
-
   const cityName = getCityNameInUrl();
-  const cityAdcode = await getCityAdcodeByCityName(cityName);
+  const cityAdcode = await getCityAdcode(cityName);
   const subwayInfo = await getSubwayInfo(cityName, cityAdcode);
-  console.log(subwayInfo);
+  const stationsCoordinates = getStationsCoordinates(subwayInfo);
 
   const mySubway = subway("mySubway", {
     easy: 0,
     adcode: cityAdcode
   });
 
-  // mySubway.event.on("subway.complete", () => {
-  //   mySubway.addInfoWindow("320100021573029", {
-  //     content: "<div>自定义内容</div>"
-  //   });
-  // });
-
-  // center station on click
-  mySubway.event.on("station.touch", function (ev, info) {
-    var id = info.id;
+  mySubway.event.on("station.touch", function(event, info) {
+    const id = info.id;
     mySubway.stopAnimation();
-    mySubway.addInfoWindow(id, {});
-    var center = mySubway.getStCenter(id);
+    mySubway.addInfoWindow(id, {
+      content: 
+      `
+      <div class="tip_bady">
+        <div class="tip_name_detail">
+          <span class="tip_name" id="tip_name">${info.name}</span>
+        </div>
+        <div class="tip_route">
+          <div>坐标: ${stationsCoordinates[id]}</div>
+        </div>
+      </div>
+      `
+    });
+    const center = mySubway.getStCenter(id);
     mySubway.setCenter(center);
   });
 };
