2d5cb2180ab5f4bdc509a2b934ea0585cee7dcab
zouyun <thunderrun2035@gmail.com>
2018-03-30 15:47:19 +0800

    add railway framework

--- index.js
+++ index.js
@@ -1,90 +1,66 @@
-const canvas = new fabric.Canvas('canvas');
+const canvas = new fabric.Canvas("canvas");
 
-const getSubwayData = (city) => fetch(`data/${city}.json`)
-  .then(res => res.json())
-  .then(res => res.l);
+// global
+const huhanrongY = 500;
+const jingguangX = 450;
 
-const createPathObj = (line) => {
-  let pathString = '';
-  line.c.forEach(drawPoint => {
-    pathString += ` L ${drawPoint}`
-  })
-  return new fabric.Path(pathString, {
+const lines = [
+  // 沪汉蓉
+  new fabric.Path("M 0 0 L 800 0", { 
+    stroke: "#00a19a",
+    strokeWidth: 6,
+    originY: "center",
+    left: 50,
+    top: huhanrongY,
+  }),
+  // 京广 + 哈大
+  new fabric.Path('M 0 0 L 0 -600 L 200 -800', {
+    stroke: "#dc1056",
+    strokeWidth: 6,
+    originY: 'bottom',
+    left: 450 - 3,
+    top: 850,
     fill: '',
-    stroke: `#${line.cl}`,
-    strokeWidth: 5
-  });
-};
+  })
+];
 
-const createCircleObj = (station) => {
-  let coordinates = station.p.split(' ');
+const getCity = (x, y, color, name, scale = 1) => {
   return new fabric.Circle({
-    radius: 4,
-    fill: 'white',
-    left: Number(coordinates[0]) - 2,
-    top: Number(coordinates[1]) - 2,
-    borderColor: 'black',
-    stroke: 'black',
-    strokeWidth: 1.5,
+    radius: 5 * scale,
+    fill: "white",
+    stroke: color,
+    strokeWidth: 2 * scale,
+    left: x,
+    top: y,
+    originX: "center",
+    originY: "center",
+    name: name
   });
 };
 
-const createTextObj = (station) => {
-  let coordinates = station.p.split(' ');
-  return new fabric.Text(station.n, {
-    fontSize: 8,
-    left: Number(coordinates[0]) + 10,
-    top: Number(coordinates[1]) - 2,
-  });
-};
+const cities = [
+  // huhanrong
+  getCity(50, huhanrongY, "#00a19a", "chengdu", 1.5),
+  getCity(200, huhanrongY, "#00a19a", "chongqing", 1.5),
+  getCity(450, huhanrongY, "#00a19a", "wuhan", 1.5),
+  getCity(600, huhanrongY, "#00a19a", "hefei"),
+  getCity(700, huhanrongY, "#00a19a", "nanjing", 1.5),
+  getCity(750, huhanrongY, "#00a19a", "wuxi"),
+  getCity(800, huhanrongY, "#00a19a", "suzhou", 1.5),
+  getCity(850, huhanrongY, "#00a19a", "shanghai", 2),
 
-const createObjects = (data) => {
-  let lines = []
-  let circles = [];
-  let texts = [];
-  data.forEach(line => {
-    lines.push(createPathObj(line))
-    line.st.forEach(station => {
-      circles.push(createCircleObj(station));
-      // texts.push(createTextObj(station));
-    });
-  });
-  return [...lines, ...circles, ...texts]
-};
+  // jingguang
+  getCity(jingguangX, 850, "#dc1056", "shenzhen", 1.5),
+  getCity(jingguangX, 800, "#dc1056", "guangzhou", 2),
+  getCity(jingguangX, 650, "#dc1056", "changsha", 1),  
+  
+];
 
-const drawSubwayMap = (city, offset, scale = 1) => {
-  getSubwayData(city).then((data) => {
-    let objects = createObjects(data);
-    let group = new fabric.Group(objects, {
-      left: offset.x,
-      top: offset.y,
-      scaleX: scale,
-      scaleY: scale
-    });
-    canvas.add(group);
-  });
-};
+canvas.add(...lines);
+canvas.add(...cities);
 
-drawSubwayMap('nanjing', {
-  x: 0,
-  y: 0
+canvas.on("mouse:down", e => {
+  if (e.target && e.target.name) {
+    location.href = `subway.html?city=${e.target.name}`;
+  }
 });
-
-drawSubwayMap('wuxi', {
-  x: 3000,
-  y: 45
-});
-
-drawSubwayMap('suzhou', {
-  x: 4500,
-  y: 0
-});
-
-drawSubwayMap('shanghai', {
-  x: 6500,
-  y: 0
-}, 1.2);
-
-let path = new fabric.Path('L 0 0 L 430 430 L 3000 430 L 3100 530 L 4000 530 L 4109 639 L 6000 639 L 7000 639 L 7051 690 L 7450 690');
-path.set({ left: 200, top: 0, strokeWidth: 8 , stroke: '#d3d3d3', fill: '',});
-canvas.add(path);
