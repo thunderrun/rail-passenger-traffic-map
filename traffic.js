501813bb051cab4890934e758ec5c08fc3a04f02
zouyun <thunderrun2035@gmail.com>
2018-04-04 16:00:04 +0800

    add graphlib

--- traffic.js
+++ traffic.js
@@ -1,5 +1,33 @@
-fetch('data/nanjing.json').then((res)=>{
-  return res.json();
-}).then((json)=>{
-  console.log(json)
-})
+(async () =>{
+  const data = await (await fetch('data/nanjing.json')).json();
+  const g = new graphlib.Graph({
+    directed: false,
+  });
+
+  console.log(data);
+
+  const lines = data.l;
+
+  lines.forEach(line => {
+    const stations = line.st;
+    stations.forEach((station)=>{
+      g.setNode(station.n, station)
+    })
+  });
+
+  console.log(g.nodes());
+
+  lines.forEach(line => {
+    const stations = line.st;
+    for (let index = 1; index < stations.length; index++) {
+      g.setEdge(stations[index-1].n, stations[index].n)
+    }
+  });
+
+  console.log(g.edges());
+  
+})();
+
+
+
+
