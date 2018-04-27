567ecf5c3df2017d5b443f1cfb3cc053f67ecbda
thunderrun <thunderrun2035@gmail.com>
2018-04-27 22:37:43 +0800

    update traffic.js

--- traffic.js
+++ traffic.js
@@ -9,6 +9,7 @@
   console.log(data);
 
   const lines = data.l;
+  let edges = [];
 
   lines.forEach(line => {
     const coordinates = line.c;
@@ -21,28 +22,51 @@
         string += `L${coordinates} `;      
       }
     });
-    map.innerHTML += `<path d="${string}" style="stroke: #${line.cl}; stroke-width: 5px;"/>`; 
+    map.innerHTML += `<path d="${string}" style="stroke: #${line.cl}; stroke-width: 5px; stroke-opacity: 0.3;"/>`; 
 
     const stations = line.st;
     stations.forEach((station)=>{
-      g.setNode(station.n, station)
-    })
+      g.setNode(station.n, station);
+      const stationTraffic = (parseInt(station.officeBuilding) + parseInt(station.residential)) * 0.2 + 4;
+      const stationPosition = station.p.split(" ");
+      map.innerHTML += `<circle cx="${stationPosition[0]}" cy="${stationPosition[1]}" r="${stationTraffic}" stroke="#${line.cl}" stroke-width="1" fill="white" />`
+    });
+
+    for (let index = 1; index < stations.length; index++) {
+        g.setEdge(stations[index-1].n, stations[index].n, `${stations[index-1].n} - ${stations[index].n}`);
+        edges.push({
+          start: stations[index-1].p, 
+          end: stations[index].p,
+          traffic: 0,
+        })
+    }
+
   });  
+
+  console.log(edges);
 
   // console.log(g.nodes());
 
-  lines.forEach(line => {
-    const stations = line.st;
-    for (let index = 1; index < stations.length; index++) {
-      g.setEdge(stations[index-1].n, stations[index].n, `${stations[index-1].n} - ${stations[index].n}`)
-    }
-  });
-
   // console.log(g.edges());
 
-  const maps = graphlib.alg.dijkstra(g, "无锡火车站", undefined, (v) => { return g.nodeEdges(v); });
+  // const maps = graphlib.alg.dijkstra(g, "东林广场", undefined, (v) => { return g.nodeEdges(v); });
 
   // console.log(maps)
+
+  lines.forEach(line => {
+    const stations = line.st;
+    stations.forEach((station)=>{
+      if (station.officeBuilding > 0) {
+        let maps = graphlib.alg.dijkstra(g, station.n, undefined, (v) => { return g.nodeEdges(v); });
+        Object.keys(maps).forEach(stationName=>{
+          if (maps[stationName].distance > 0 ) {
+            let shortestPath = [];
+            // nextStation = maps[stationName].predecessor
+          }
+        });
+      }
+    })
+  })
   
 })();
 
