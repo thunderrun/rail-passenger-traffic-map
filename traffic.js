2d4965abb3653d44a5fdf5e1f92716e9e93f83d0
thunderrun <thunderrun2035@gmail.com>
2018-05-04 12:51:01 +0800

    update traffic algorism

--- traffic.js
+++ traffic.js
@@ -9,6 +9,7 @@
   console.log(data);
 
   const lines = data.l;
+  let stationsData = {};
   let edges = [];
 
   lines.forEach(line => {
@@ -30,13 +31,20 @@
       const stationTraffic = (parseInt(station.officeBuilding) + parseInt(station.residential)) * 0.2 + 4;
       const stationPosition = station.p.split(" ");
       map.innerHTML += `<circle cx="${stationPosition[0]}" cy="${stationPosition[1]}" r="${stationTraffic}" stroke="#${line.cl}" stroke-width="1" fill="white" />`
+      // seperate station data
+      stationsData[station.n] = {
+        officeBuilding: parseInt(station.officeBuilding),
+        residential: parseInt(station.residential),
+      }
     });
 
     for (let index = 1; index < stations.length; index++) {
         g.setEdge(stations[index-1].n, stations[index].n, `${stations[index-1].n} - ${stations[index].n}`);
         edges.push({
-          start: stations[index-1].p, 
-          end: stations[index].p,
+          startStation: stations[index-1].n,
+          endStation: stations[index].n,
+          startCoordinates: stations[index-1].p,
+          endCoordinates: stations[index].p,
           traffic: 0,
         })
     }
@@ -44,7 +52,7 @@
   });  
 
   console.log(edges);
-
+  
   // console.log(g.nodes());
 
   // console.log(g.edges());
@@ -53,21 +61,63 @@
 
   // console.log(maps)
 
+  console.log(stationsData);
+
   lines.forEach(line => {
     const stations = line.st;
     stations.forEach((station)=>{
       if (station.officeBuilding > 0) {
         let maps = graphlib.alg.dijkstra(g, station.n, undefined, (v) => { return g.nodeEdges(v); });
         Object.keys(maps).forEach(stationName=>{
-          if (maps[stationName].distance > 0 ) {
-            let shortestPath = [];
-            // nextStation = maps[stationName].predecessor
+          if (maps[stationName].distance > 0 && stationsData[stationName].residential > 0 ) {
+            let previousStation = maps[stationName].predecessor;
+            let shortestPath = [stationName, previousStation];
+            while ( previousStation !== station.n ) {
+              previousStation = maps[previousStation].predecessor;
+              shortestPath.push(previousStation)
+            }
+            let distance = shortestPath.length;
+            for(let index = 0; index < distance; index++){
+              let edgesIndex = edges.findIndex(function(edge){
+                return (edge.startStation === shortestPath[index] && edge.endStation === shortestPath[index+1]) ||
+                (edge.endStation === shortestPath[index] && edge.startStation === shortestPath[index+1]);
+              })
+              if(edgesIndex > -1) {
+                edges[edgesIndex].traffic += 1/distance * station.officeBuilding * stationsData[stationName].residential;                
+              }
+            }
           }
         });
       }
     })
-  })
-  
+  });
+
+  console.log(edges);
+
+  lines.forEach(line => {
+    const coordinates = line.c;
+
+    let startCoordinates = ``;
+    let string = '';
+    line.c.forEach((coordinates, index) => {
+      // bug
+      let realStart = edges.find(function(edge){ edge.startCoordinates === coordinates || edge.endCoordinates === coordinates });
+      if (index === 0) {
+        startCoordinates = coordinates;
+        string += `M${coordinates} `
+      }else{
+        string += `L${coordinates} `;
+        let interval = edges.find(function(edge){
+          return (edge.startCoordinates === coordinates || edge.endCoordinates === coordinates) && ( edge.startCoordinates === startCoordinates || edge.endCoordinates === startCoordinates );
+        })
+        if(interval){
+          map.innerHTML += `<path d="${string}" style="stroke: #${line.cl}; stroke-width: ${interval.traffic * 0.002}px;"/>`;         
+          startCoordinates = coordinates;
+          string = `M${coordinates} `;
+        }  
+      }
+    });
+  });
 })();
 
 
